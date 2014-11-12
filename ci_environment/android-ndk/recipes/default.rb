39a008754c6a7df8c09368c9602773d1bd0d7b41
Hiro Asari <asari.ruby@gmail.com>
2014-11-12 15:28:36 -0500

    Update Android NDK recipe to r10c

--- ci_environment/android-ndk/recipes/default.rb
+++ ci_environment/android-ndk/recipes/default.rb
@@ -1,24 +1,26 @@
 setup_root        = node['android-ndk']['setup_root'].to_s.empty? ? node['ark']['prefix_home'] : node['android-ndk']['setup_root']
 android_ndk_home  = File.join(setup_root, node['android-ndk']['name'])
+download_file_to  = [setup_root, File.basename(node['android-ndk']['download_url'])].join(File::SEPARATOR)
 
-#
-# Download and setup android-sdk tarball package
-#
-ark node['android-ndk']['name'] do
-  url         node['android-ndk']['download_url']
-  checksum    node['android-ndk']['checksum']
-  version     node['android-ndk']['version']
-  prefix_root node['android-ndk']['setup_root']
-  prefix_home node['android-ndk']['setup_root']
-  owner       node['android-ndk']['owner']
-  group       node['android-ndk']['group']
+remote_file download_file_to do
+  source node['android-ndk']['download_url']
+  mode 0755
+end
+
+execute 'Expand Android NDK archive' do
+  command       "./#{File.basename(node['android-ndk']['download_url'])} >/dev/null"
+  user          node['android-ndk']['owner']
+  group         node['android-ndk']['group']
 end
 
 # TODO find a way to handle 'chmod stuff' below with own chef resource (idempotence stuff...)
-execute 'Grant all users to read android files' do
-  command       "chmod -R a+r #{android_ndk_home}/*"
+bash 'Grant all users to read android files' do
   user          node['android-ndk']['owner']
   group         node['android-ndk']['group']
+  code <<-EOF
+    mv #{setup_root}/android-ndk-#{node['android-ndk']['version']} #{android_ndk_home}
+    chown -R #{node['android-ndk']['owner']}:#{node['android-ndk']['group']} #{android_ndk_home}
+  EOF
 end
 
 #
